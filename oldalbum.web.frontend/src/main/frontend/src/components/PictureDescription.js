82f6262502a5b735c7803a5a1349008cbe1776ca
Steinar Bang <sb@dod.no>
2026-02-10 23:53:01 +0100

    Edit picture description in place

--- oldalbum.web.frontend/src/main/frontend/src/components/PictureDescription.js
+++ oldalbum.web.frontend/src/main/frontend/src/components/PictureDescription.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { usePostModifypictureMutation } from '../api';
 
 function PictureDescription(props) {
     const { className, item, metadata } = props;
+    const canModifyAlbum = useSelector(state => state.canModifyAlbum);
+    const loggedIn = useSelector(state => state.loggedIn);
+    const editMode = useSelector(state => state.editMode);
+    const editable = canModifyAlbum && loggedIn && editMode;
+    const [ postModifypicture ] = usePostModifypictureMutation();
+    const onSaveDescription = async (e) =>
+          {
+              if (e.key === 'Enter') {
+                  await postModifypicture({ ...item, description: e.target.innerText });
+                  e.target.blur();
+              }
+          }
 
     if (!item.description && !metadata) {
         return null;
@@ -9,7 +23,10 @@ function PictureDescription(props) {
 
     return (
         <div className={className}>
-            <div className="alert alert-primary d-flex justify-content-center" role="alert">{item.description} {metadata}</div>
+            <div className="alert alert-primary d-flex justify-content-center" role="alert">
+                <span contentEditable={editable} onKeyDown={onSaveDescription}>{item.description}</span>
+                {metadata}
+            </div>
         </div>
     );
 }
